02f7a2bb37ba83229d6800aa2707cfdf32514bdc
Hyblocker <hyblocker@protonmail.com>
2024-02-23 12:55:07 +0100

    Internal glove state

--- src/openvr_driver/contactglove_device.hpp
+++ src/openvr_driver/contactglove_device.hpp
@@ -2,6 +2,33 @@
 
 #include "openvr_driver.h"
 
+// Internal state of a glove as tracked by the driver
+struct ContactGloveState {
+	// Fingers
+	float thumbRoot;
+	float thumbTip;
+	float indexRoot;
+	float indexTip;
+	float middleRoot;
+	float middleTip;
+	float ringRoot;
+	float ringTip;
+	float pinkyRoot;
+	float pinkyTip;
+	// Input
+	bool hasMagnetra;
+	bool systemUp;
+	bool systemDown;
+	bool buttonUp;
+	bool buttonDown;
+	bool joystickClick;
+	float joystickX;
+	float joystickY;
+	// Battery
+	uint8_t battery;
+};
+
+// SteamVR tracked device
 class ContactGloveDevice {
 
 };
